1608c5ce79db00db23dd2794b9e308fdb3a7a698
Diana Borja <valeriaborja975@gmail.com>
2025-06-19 09:01:56 -0500

    Estructuras No Lineales

--- src/App.java
+++ src/App.java
@@ -5,20 +5,31 @@ public class App {
         ArbolBinario ab = new ArbolBinario();
         ab.insert(50);
         ab.insert(17);
-        ab.insert(16);
+        ab.insert(76);
         ab.insert(9);
         ab.insert(23);
         ab.insert(54);
         ab.insert(14);
         ab.insert(19);
 
-        ab.imprimirArbol();
+    System.out.println("Nombre : Diana Borja");
+    System.out.println();
+    //ab.imprimirArbol();
+    //ab.buscar(23);  
+    System.out.println("La altura es: " + ab.getHeight());
+    System.out.println("El peso del arbol es: " + ab.getWeight());
+    System.out.println();
+    ab.printInOrderWithHeight();
+    System.out.println();
+    ab.printFactorDeEquilibrio();
+    System.out.println();
+    ab.arbolEquilibrado(0);
+    System.out.println();
+    ab.printFactorDeEquilibrio2();
+    System.out.println();
+    ab.nodosDesequilibrados();
 
-        ab.buscar(23);
-        
-        
-        if(!ab.buscar(77)){
-            System.out.println("No encontro");
-        }
+
+       
     }
 }
--- src/Materia/controllers/ArbolBinario.java
+++ src/Materia/controllers/ArbolBinario.java
@@ -31,7 +31,7 @@ public class ArbolBinario {
     public void imprimir(Node node){
         if (node != null){
             imprimir(node.getRight());
-            System.out.print(node.getValue() + ", ");
+            System.out.println(node.getValue() + ", ");
             imprimir(node.getLeft());
         }
     }
@@ -49,6 +49,150 @@ public class ArbolBinario {
             return buscarRec(node.getLeft(), value);
         } else {
             return buscarRec(node.getRight(), value);
-        }   
+        }  
     }
+    
+    public int getHeight(){
+        return getHeightRec(root);
+
+    }
+    
+    private int getHeightRec(Node node){
+        if(node == null){
+            return 0;
+        }
+        int leftHeight = getHeightRec(node.getLeft());
+        int rightHeight = getHeightRec(node.getRight());
+        return Math.max(leftHeight+1, rightHeight+1);
+    }
+
+    public int getWeight(){
+        return getWeightRec(root);
+    }
+    private int getWeightRec(Node node){
+        if(node == null){
+            return 0;
+        }
+        int leftWeight = getWeightRec(node.getLeft());
+        int rightWeight = getWeightRec(node.getRight());
+
+        return leftWeight + rightWeight + 1;
+    }
+
+    public void printInOrderWithHeight(){
+        System.out.println("Arbol In Order con Alturas: ");
+        printInOrderWithHeightRec(root);
+    }
+
+    private void printInOrderWithHeightRec (Node node){
+        if(node == null){
+            return ; 
+        }
+        printInOrderWithHeightRec(node.getLeft());
+        int height = getHeightRec(node);
+        System.out.print(node.getValue() + " (h = " + height + ")" + " , ");
+        System.out.println();
+        printInOrderWithHeightRec(node.getRight());
+    }
+
+    public int calcularFacEquilibrio(){
+        return calcularFacEquilibrioRec(root);
+    }
+
+    private int calcularFacEquilibrioRec(Node node){
+        if(node == null){
+            return 0;
+        }
+        int lefheight = getHeightRec(node.getLeft());
+        int rightHeight = getHeightRec(node.getRight());
+    
+        return lefheight - rightHeight;
+    }
+    public void printFactorDeEquilibrio(){
+        System.out.println("Arbol In Order Con Factor de Equilibrio: ");
+        printFactorDeEquilibrioRec(root);
+    }
+    private void printFactorDeEquilibrioRec(Node node){
+        if(node == null){
+            return;
+        }
+        printFactorDeEquilibrioRec(node.getLeft());
+        int factor = calcularFacEquilibrioRec(node);
+        System.out.print(node.getValue() +  " (bf = " + factor + ")" + " , ");
+        System.out.println();
+        printFactorDeEquilibrioRec(node.getRight());
+    }
+
+    public boolean arbolEquilibrado(int value){
+        boolean equilibrado = arbolEquilibradoRec(root);
+        
+        System.out.println("Arbol esta equilibrado = " + equilibrado);
+        if(equilibrado == true){
+            int nuevoValor = 15;
+            insert(nuevoValor);
+            System.out.println("Agregamos el nuevo valor: " + nuevoValor);
+        }
+        return equilibrado;
+    }
+    private boolean arbolEquilibradoRec(Node node){
+        if(node == null){
+            return true;
+        }
+        int leftHeight = getHeightRec(node.getLeft());
+        int rightHeight = getHeightRec(node.getRight());
+        int balance = Math.abs(leftHeight - rightHeight);
+        return (balance <= 1) &&
+            arbolEquilibradoRec(node.getLeft()) &&
+            arbolEquilibradoRec(node.getRight());
+    }
+
+      public int calcularFacEquilibrio2(){
+        return calcularFacEquilibrioRec2(root);
+    }
+
+    private int calcularFacEquilibrioRec2(Node node){
+        if(node == null){
+            return 0;
+        }
+        int lefheight = getHeightRec(node.getLeft());
+        int rightHeight = getHeightRec(node.getRight());
+    
+        return lefheight - rightHeight;
+    }
+
+     public void printFactorDeEquilibrio2(){
+        System.out.println("Arbol In Order Con Factor de Equilibrio: ");
+        printFactorDeEquilibrioRec2(root);
+    }
+    private void printFactorDeEquilibrioRec2(Node node){
+        if(node == null){
+            return;
+        }
+        printFactorDeEquilibrioRec2(node.getLeft());
+        int factor = calcularFacEquilibrioRec2(node);
+        System.out.print(node.getValue() +  " (bf = " + factor + ")" + " , ");
+        printFactorDeEquilibrioRec2(node.getRight());
+    }
+
+    public int nodosDesequilibrados(){
+        int cantidad = nodosDesequilibradosRec(root);
+        System.out.println("Nodos desequilibrados: " + cantidad);
+        return cantidad;
+
+    }
+    private int nodosDesequilibradosRec(Node node) {
+    if (node == null) {
+        return 0;
+    }
+    int left = nodosDesequilibradosRec(node.getLeft());
+    int right = nodosDesequilibradosRec(node.getRight());
+    int bf = calcularFacEquilibrioRec(node);
+    boolean desequilibrado = Math.abs(bf) > 1;
+    return (desequilibrado ? 1 : 0) + left + right;
+}
+
+
+
+
+    
 }
